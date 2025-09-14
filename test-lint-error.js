81d8aa744886154f58d8772626e4f88e0fed44be
Daniel Githiomi <danielgithiomi@gmail.com>
2025-09-14 19:29:52 +0400

    Test (Root-Project) 'TNF-002': Testing Husky hooks with linting errors

---
+++ test-lint-error.js
@@ -0,0 +1,6 @@
+// This file has intentional linting errors
+const unusedVariable = "this should trigger a warning";
+console.log("This should also trigger a warning");
+
+// Missing semicolon
+const anotherVariable = "test"
