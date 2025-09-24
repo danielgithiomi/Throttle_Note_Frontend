e1f2d676b51075d2ddc06bbc3b591f6cd3f84210
Daniel Githiomi <danielgithiomi@gmail.com>
2025-09-25 00:32:32 +0400

    Changes made to babel files and refactored project structure

--- babel.config.js
+++ babel.config.js
@@ -1,44 +1,42 @@
 module.exports = function (api) {
-    api.cache(true);
+  api.cache(true);
 
-    const isStorybookEnabled = process.env.STORYBOOK_ENABLED === "true";
+  const isStorybookEnabled = process.env.STORYBOOK_ENABLED === "true";
 
-    const commonAliases = {
-        "@": "./",
-        "@tn/constants": "./packages/constants",
-        "@tn/hooks": "./packages/hooks",
-        "@constants": "./packages/constants/index",
-        "@hooks": "./packages/hooks/index"
-    };
+  const commonAliases = {
+    "@": "./",
+    "@tn/constants": "./packages/constants",
+    "@tn/hooks": "./packages/hooks",
+    "@constants": "./packages/constants/index",
+    "@hooks": "./packages/hooks/index",
+  };
 
-    const commonPlugins = [
-        [
-            "module-resolver",
-            {
-                extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-                alias: commonAliases,
-            },
-        ],
-        "@babel/plugin-transform-class-static-block"
-    ];
+  const commonPlugins = [
+    [
+      "module-resolver",
+      {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        alias: commonAliases,
+      },
+    ],
+    "@babel/plugin-transform-class-static-block",
+  ];
 
-    if (isStorybookEnabled) {
-        // Storybook configuration with NativeWind
-        return {
-            presets: [
-                ["babel-preset-expo", {jsxImportSource: "nativewind"}],
-                "nativewind/babel",
-            ],
-            plugins: commonPlugins,
-        };
-    }
-
-    // Regular app configuration with NativeWind
+  if (isStorybookEnabled) {
     return {
-        presets: [
-            ["babel-preset-expo", {jsxImportSource: "nativewind"}],
-            "nativewind/babel",
-        ],
-        plugins: commonPlugins,
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
+      plugins: commonPlugins,
     };
+  }
+
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: commonPlugins,
+  };
 };
